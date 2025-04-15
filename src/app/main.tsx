c523a4ad678569aa36ca6cbd2bfdfe1a9742beba
emiaesthetic <emi.aesthetic@gmail.com>
2025-04-15 10:44:39 +0500

    fix(deploy): configure client-side routing for Vercel

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -10,7 +10,7 @@ import './styles/main.css';
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </Provider>,
